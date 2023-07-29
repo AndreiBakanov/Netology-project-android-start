39af8bb562fcad0895a65cb58a5d941f4080a860
Andrei Bakanov <andrey141153@gmail.com>
2023-07-29 14:26:26 +0300

    Room try 1

--- app/src/main/java/ru/netology/nmedia/presentation/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/presentation/PostViewModel.kt
@@ -3,11 +3,9 @@ package ru.netology.nmedia.presentation
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import ru.netology.nmedia.db.AppDb
 import ru.netology.nmedia.dto.Post
-import ru.netology.nmedia.repository.PostRepositoryFileImpl
-import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
-import ru.netology.nmedia.repository.PostRepositorySharedPrefsImpl
+import ru.netology.nmedia.repository.PostRepositoryRoomImpl
 
 private val empty = Post(
     id = 0,
@@ -22,7 +20,9 @@ private val empty = Post(
 
 class PostViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val repository: PostRepository = PostRepositoryFileImpl(application)
+    private val repository: PostRepository = PostRepositoryRoomImpl(
+        AppDb.getInstance(context = application).postDao()
+    )
     val data = repository.getAll()
     val edited = MutableLiveData(empty)
 
